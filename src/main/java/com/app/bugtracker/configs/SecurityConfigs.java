1f469d7cf9533a295f98fcdb8939adaa445cb6d1
lio8n8 <lio8n8@gmail.com>
2019-06-09 13:07:21 +0300

    Security configs - Disable

--- src/main/java/com/app/bugtracker/configs/SecurityConfigs.java
+++ src/main/java/com/app/bugtracker/configs/SecurityConfigs.java
@@ -1,5 +1,33 @@
 package com.app.bugtracker.configs;
 
-public class SecurityConfigs {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigs extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/").permitAll();
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/app/bugtracker/configs/WebConfigs.java
+++ src/main/java/com/app/bugtracker/configs/WebConfigs.java
@@ -1,14 +1,5 @@
 package com.app.bugtracker.configs;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+public class WebConfigs {
 
-@Configuration
-public class WebConfigs extends WebSecurityConfigurerAdapter {
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }

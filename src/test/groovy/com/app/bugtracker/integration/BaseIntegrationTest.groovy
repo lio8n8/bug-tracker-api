54b9c57e481658bc9bd5193f6d7706fa7526cd01
lio8n8 <lio8n8@gmail.com>
2019-07-30 09:07:19 +0300

    Base integration test

---
+++ src/test/groovy/com/app/bugtracker/integration/BaseIntegrationTest.groovy
@@ -0,0 +1,12 @@
+package com.app.bugtracker.integration
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.test.context.ActiveProfiles
+
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles('integration-test')
+class BaseIntegrationTest extends Specification {
+}

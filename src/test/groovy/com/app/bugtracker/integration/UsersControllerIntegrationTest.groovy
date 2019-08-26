4a84aaca4d1e7d000f6530c2a7c14d0fa19878a3
lio8n8 <lio8n8@gmail.com>
2019-08-26 09:49:22 +0300

    Tests - Get users

--- src/test/groovy/com/app/bugtracker/integration/UsersControllerIntegrationTest.groovy
+++ src/test/groovy/com/app/bugtracker/integration/UsersControllerIntegrationTest.groovy
@@ -9,7 +9,10 @@ import com.app.bugtracker.constants.Urls
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.hateoas.PagedResources
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import spock.lang.Shared
@@ -43,6 +46,27 @@ class UsersControllerIntegrationTest extends BaseIntegrationTest {
                 new HttpEntity<>(TestUtils.getAuthHttpHeaders(user.email)), User.class)
 
         then: 'It should return a user info'
-        result.getBody().id == user.id
+        result.statusCode == HttpStatus.OK && result.getBody().id == user.id
+    }
+
+    def 'Get users'() {
+        given: 'A user with admin role'
+        def user = usersRepository.save(User.builder()
+                .email(faker.internet().emailAddress())
+                .psw(bCryptPasswordEncoder.encode(faker.internet().password()))
+                .roles([UserRoles.ADMIN] as Set)
+                .build())
+        def skip = 0
+        def limit = 100
+
+        when: 'Finds all users'
+        def result = restTemplate.exchange("${USERS_URL}?skip=${skip}&limit=${limit}", GET,
+                new HttpEntity<>(TestUtils.getAuthHttpHeaders(user.email)),
+                new ParameterizedTypeReference<PagedResources<User>>() {})
+
+        then: 'It should return a list of users'
+        result.statusCode == HttpStatus.OK &&
+                !result.getBody().getContent().isEmpty() &&
+                result.getBody().content.contains(user)
     }
 }

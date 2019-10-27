91f5a1d47c21be5fbce5e415fc522637e988bd95
lio8n8 <lio8n8@gmail.com>
2019-10-27 23:13:47 +0200

    Added WebTestClient for integration tests

--- src/test/groovy/com/app/bugtracker/integration/BaseIntegrationTest.groovy
+++ src/test/groovy/com/app/bugtracker/integration/BaseIntegrationTest.groovy
@@ -1,13 +1,20 @@
 package com.app.bugtracker.integration
 
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
 
 import spock.lang.Specification
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles('integration-test')
+import com.app.bugtracker.BugTrackerApplication
+
+@ActiveProfiles('test')
+@SpringBootTest(classes = BugTrackerApplication,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BaseIntegrationTest extends Specification {
-    final static protected BASE_URL = 'http://127.0.0.1:8080/api'   
+
+    protected WebTestClient webTestClient = WebTestClient
+            .bindToServer()
+            .baseUrl('http://127.0.0.1:8081')
+            .build()
 }
